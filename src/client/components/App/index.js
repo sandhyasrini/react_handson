b65a8b7bb8c51ae8e05c6cac1f374e6e7745cc9f
sandhyasrini <sandy150225@gmail.com>
2019-03-07 18:20:28 +0530

    data changes

--- src/client/components/App/index.js
+++ src/client/components/App/index.js
@@ -10,9 +10,13 @@ import { FaFacebookF , FaTwitter ,FaInstagram } from "react-icons/fa";
 class App extends Component {
   render() {
     return (
-        <div style= {
+        <div className="bgImage" style= {
           {  background:"linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.7)),url(" +bgImage+")",
-          height:"100vh"
+          backgroundPosition: "center",
+          backgroundRepeat:"no-repeat",
+          backgroundSize:"cover"
+
+
         }
         }>
             <Header />
